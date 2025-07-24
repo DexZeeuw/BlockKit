da7e45730d72e13fb5c73d3a1b35c632e027d9b4
ruben <rubendalebout@gmail.com>
2025-07-24 19:47:15 +0200

    stop scoreboard flickering

--- blockkit-core/src/main/java/com/blockkit/core/scoreboard/renderer/TeamBasedRenderer.java
+++ blockkit-core/src/main/java/com/blockkit/core/scoreboard/renderer/TeamBasedRenderer.java
@@ -1,6 +1,5 @@
 package com.blockkit.core.scoreboard.renderer;
 
-import com.blockkit.BlockKit;
 import me.clip.placeholderapi.PlaceholderAPI;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
@@ -15,36 +14,37 @@ public class TeamBasedRenderer implements ScoreboardRenderer {
     public void render(Objective obj, Player player, List<String> lines) {
         Scoreboard board = obj.getScoreboard();
 
-        // 1) Cleanup
-        board.getTeams().forEach(Team::unregister);
-        board.getEntries().forEach(board::resetScores);
-
-        // 2) Render elke regel
         int size = lines.size();
         for (int i = 0; i < size; i++) {
-            // originele tekst
-            String text = lines.get(i);
+            // unieke onzichtbare entry (§0 … §f)
+            ChatColor code = ChatColor.values()[i];
+            String entryId = code.toString();
+            String teamId  = "line_" + i;
 
-            // run PAPI als beschikbaar
+            // haal of maak team
+            Team team = board.getTeam(teamId);
+            if (team == null) {
+                team = board.registerNewTeam(teamId);
+                team.addEntry(entryId);
+            }
+
+            // haal tekst & pas PAPI toe indien nodig
+            String text = lines.get(i);
             if (Bukkit.getPluginManager().isPluginEnabled("PlaceholderAPI")) {
                 text = PlaceholderAPI.setPlaceholders(player, text);
             }
 
-            // unieke, onzichtbare entry (§0…§f)
-            ChatColor code = ChatColor.values()[i];
-            String entryId  = code.toString();
-            String teamId   = "line_" + i;
+            // update prefix alleen als gewijzigd
+            if (!text.equals(team.getPrefix())) {
+                team.setPrefix(text);
+            }
 
-            // registreer team
-            Team team = board.registerNewTeam(teamId);
-            team.addEntry(entryId);
-
-            // prefix = jouw (RGB-)tekst
-            team.setPrefix(text);
-            team.setSuffix("");
-
-            // score voor volgorde
-            obj.getScore(entryId).setScore(size - i);
+            // update score als die niet klopt
+            int expectedScore = size - i;
+            Score current = obj.getScore(entryId);
+            if (current.getScore() != expectedScore) {
+                current.setScore(expectedScore);
+            }
         }
     }
 }
